17793540cdb11d5130710c27a3f3476ab8686a65
Michal <zawadzki.micha@gmail.com>
2021-05-19 11:00:38 +0200

    Add dj_database

--- Quizoku/settings.py
+++ Quizoku/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'].update(os.environ.get('DATABASE_URL'))
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
