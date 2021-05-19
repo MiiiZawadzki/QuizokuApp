c5d2783acf0bec3d843d04bbb4a300c1a7c86318
Michal <zawadzki.micha@gmail.com>
2021-05-19 11:07:39 +0200

    Change db settings

--- Quizoku/settings.py
+++ Quizoku/settings.py
@@ -85,8 +85,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
